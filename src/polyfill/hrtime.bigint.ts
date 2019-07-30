0d2fa08eab44ba39c8778a556cd46e9a13126d06
Aaron Turner <aaron@aaronthedev.com>
2019-07-29 17:01:00 -0700

    Fixed offline changes again

--- src/polyfill/hrtime.bigint.ts
+++ src/polyfill/hrtime.bigint.ts
@@ -11,16 +11,9 @@ if (process.hrtime && process.hrtime.bigint) {
   const baseTime: [number, number] = process.hrtime()
   hrtime = () => {
     const diff = process.hrtime(baseTime)
-    baseTime[0] += diff[0]
-    baseTime[1] += diff[1]
-
-    while (baseTime[1] >= 10e9) {
-      baseTime[1] -= 10e9
-      baseTime[0] += 1
-    }
 
     // Return the time
-    return ((baseTime[0] * NS_PER_SEC + baseTime[1]) as unknown) as bigint
+    return ((diff[0] * NS_PER_SEC + diff[1]) as unknown) as bigint
   }
 }
 
