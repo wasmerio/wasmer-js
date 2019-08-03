3ecf9ea5fec4b8c8c8604fe84c0e68855d0f9686
Aaron Turner <aaron@aaronthedev.com>
2019-08-02 18:02:47 -0700

    Left another note

--- examples/wapm-shell/workers/process/asyncify.js
+++ examples/wapm-shell/workers/process/asyncify.js
@@ -3,3 +3,5 @@
 // Do asyncify stuff here
 // TODO: Will have to move module compilation to here :p
 // https://github.com/AssemblyScript/binaryen.js/#module-construction
+
+// Actually, maybe we don't need binaryen, we may just need to push some bytes for the stack unwinder somewhere whenever we encourter something?
