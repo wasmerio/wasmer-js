4a3cf0c0a5e04184e9dcd9e0a524e14e09d436df
Aaron Turner <aaron@aaronthedev.com>
2019-08-02 17:54:36 -0700

    Started an asyncify function

---
+++ examples/wapm-shell/workers/process/asyncify.js
@@ -0,0 +1,5 @@
+// https://kripken.github.io/blog/wasm/2019/07/16/asyncify.html
+
+// Do asyncify stuff here
+// TODO: Will have to move module compilation to here :p
+// https://github.com/AssemblyScript/binaryen.js/#module-construction
