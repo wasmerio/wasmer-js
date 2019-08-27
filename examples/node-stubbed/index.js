31e0511c9ca299e415080aa6e1e442c99776a54b
Aaron Turner <aaron@aaronthedev.com>
2019-08-27 15:42:48 -0700

    Duk works in node?

--- examples/node-stubbed/index.js
+++ examples/node-stubbed/index.js
@@ -5,39 +5,84 @@ const { WASI } = require("../../dist/index.cjs.js");
 const wasiJsTransformer = require("./wasi-js-transformer");
 const WasmerFileSystem = require("../../dist/examples/file-system/file-system.cjs.js");
 const argv = require("minimist")(process.argv.slice(2));
+const chalk = require("chalk");
+var readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  terminal: false
+});
+let currentStdinLine = "";
 
-const fsSpyLog = action => {
-  console.log(" ");
-  console.log(" ");
-  console.log("[FS Spy]:");
-  console.log(action.method);
-  console.log(action.args);
+// Check if we passed a wasm file
+if (argv._.length < 1) {
+  throw new Error("You must pass in a wasm file as an argument");
+}
+
+const msleep = n => {
+  Atomics.wait(new Int32Array(new SharedArrayBuffer(4)), 0, 0, n);
 };
 
-const nodeFs = spy(fs, action => {
-  fsSpyLog(action);
-});
+// Set up our file system (NOTE: We could use node's fs normally for this case)
 const wasmerFs = new WasmerFileSystem();
+//stdin reading
+let stdinReadCounter = 0;
+const stdinRead = (stdinBuffer, offset, length, position) => {
+  // For some reason, read is called 3 times per actual read
+  // Thus we have a counter to handle this.
+  // TODO: This should only be needed if we are prompting, and not needed for piping.
+  if (stdinReadCounter !== 0) {
+    if (stdinReadCounter < 2) {
+      stdinReadCounter++;
+    } else {
+      stdinReadCounter = 0;
+    }
+    return 0;
+  }
+
+  // Since reading will keep requesting data, we need to give end of file
+  stdinReadCounter++;
+
+  // Wait for stdin
+  let responseStdin = null;
+  // Using deasync to simply allow the event loop to run but pause here: https://github.com/abbr/deasync
+  require("deasync").loopWhile(() => currentStdinLine.length < 1);
+  responseStdin = currentStdinLine;
+  currentStdinLine = "";
+
+  // First check for errors
+  if (!responseStdin) {
+    return 0;
+  }
+
+  const buffer = new TextEncoder().encode(responseStdin);
+  for (let x = 0; x < buffer.length; ++x) {
+    stdinBuffer[x] = buffer[x];
+  }
+
+  return buffer.length;
+};
+wasmerFs.volume.fds[0].read = stdinRead.bind(this);
 // stdout / error writing
 const stdoutWrite = buffer => {
+  console.log(buffer.toString() + "\r\n");
   return buffer.length;
 };
 wasmerFs.volume.fds[1].write = stdoutWrite.bind(this);
 wasmerFs.volume.fds[2].write = stdoutWrite.bind(this);
 wasmerFs.fs = spy(wasmerFs.fs, action => {
-  fsSpyLog(action);
+  console.log(
+    chalk.blue(`
+  [FS Spy]: ${action.method}
+  [FS Spy]: ${action.args}
+  `)
+  );
 });
 
-let boundFs = nodeFs;
-if (argv.fs && argv.fs.startsWith("wasmer")) {
-  boundFs = wasmerFs.fs;
-}
-
 const wasi = new WASI({
-  // preopenDirectories: { '.': '.' },
   bindings: {
     ...WASI.defaultBindings,
-    fs: boundFs
+    fs: wasmerFs.fs
   }
 });
 
@@ -47,18 +92,15 @@ Object.keys(wasi.exports).forEach(wasiExportKey => {
   wasi.exports[wasiExportKey] = sinon
     .stub(wasi.exports, wasiExportKey)
     .callsFake(function() {
-      console.log(" ");
-      console.log(" ");
-      console.log("[Wasi Stub]:");
-      console.log(wasiExportKey);
-      console.log(arguments);
+      console.log(
+        chalk.green(`
+      [Wasi Stub]: ${wasiExportKey}
+      [Wasi Stub]: ${JSON.stringify(arguments)}
+      `)
+      );
       return originalWasiExport.apply(null, arguments);
     });
 });
-
-if (argv._.length < 1) {
-  throw new Error("You must pass in a wasm file as an argument");
-}
 
 // Read in the input wasm file
 const wasmBuffer = fs.readFileSync(argv._[0]);
@@ -73,6 +115,11 @@ const asyncTask = async () => {
   });
   const inst = response.instance;
   wasi.setMemory(inst.exports.memory);
+
+  // Take in stdin
+  rl.on("line", line => {
+    currentStdinLine = line;
+  });
 
   try {
     inst.exports._start();
