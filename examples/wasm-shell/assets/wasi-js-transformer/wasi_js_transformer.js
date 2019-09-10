67b77290574295c92bed0bbbaf6504e38248c89b
Aaron Turner <aaron@aaronthedev.com>
2019-09-10 14:03:58 -0700

    Cleaned up the install process, and cleaned up a lot of loose ends

--- examples/wasm-shell/assets/wasi-js-transformer/wasi_js_transformer.js
+++ examples/wasm-shell/assets/wasi-js-transformer/wasi_js_transformer.js
@@ -73,19 +73,25 @@ function init(module) {
   };
 
   if (
-    module instanceof URL ||
+    (typeof URL === "function" && module instanceof URL) ||
     typeof module === "string" ||
-    module instanceof Request
+    (typeof Request === "function" && module instanceof Request)
   ) {
     const response = fetch(module);
     if (typeof WebAssembly.instantiateStreaming === "function") {
       result = WebAssembly.instantiateStreaming(response, imports).catch(e => {
-        console.warn(
-          "`WebAssembly.instantiateStreaming` failed. Assuming this is because your server does not serve wasm with `application/wasm` MIME type. Falling back to `WebAssembly.instantiate` which is slower. Original error:\n",
-          e
-        );
         return response
-          .then(r => r.arrayBuffer())
+          .then(r => {
+            if (r.headers.get("Content-Type") != "application/wasm") {
+              console.warn(
+                "`WebAssembly.instantiateStreaming` failed because your server does not serve wasm with `application/wasm` MIME type. Falling back to `WebAssembly.instantiate` which is slower. Original error:\n",
+                e
+              );
+              return r.arrayBuffer();
+            } else {
+              throw e;
+            }
+          })
           .then(bytes => WebAssembly.instantiate(bytes, imports));
       });
     } else {
