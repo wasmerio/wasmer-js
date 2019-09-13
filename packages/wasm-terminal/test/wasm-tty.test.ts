56b14eaf48040908f8bb6090f2b1d1a089a45051
Aaron Turner <aaron@aaronthedev.com>
2019-09-13 14:56:11 -0700

    Wrote some tests for the tty

--- packages/wasm-terminal/test/wasm-tty.test.ts
+++ packages/wasm-terminal/test/wasm-tty.test.ts
@@ -1,5 +1,39 @@
+import WasmTTY from "../lib/wasm-tty/wasm-tty";
+
 describe("WasmTty", () => {
-  it("should pass", async () => {
-    expect(true).toBe(true);
+  let wasmTty: WasmTTY;
+  let xtermMock: any;
+
+  beforeEach(async () => {
+    xtermMock = {
+      write: jest.fn(() => {}),
+      cols: 10,
+      rows: 10
+    };
+
+    // @ts-ignore
+    wasmTty = new WasmTTY(xtermMock);
+    wasmTty.println = jest.fn(message => wasmTty.print(message));
+  });
+
+  it("should print", async () => {
+    wasmTty.print("Wasmer");
+    expect(xtermMock.write.mock.calls.length).toBe(1);
+  });
+
+  it("should print wide", async () => {
+    wasmTty.printWide(["Yo", "Yoo"], 2);
+    expect(xtermMock.write.mock.calls.length).toBe(1);
+    expect(xtermMock.write.mock.calls[0][0]).toBe("Yo   Yoo  ");
+  });
+
+  it("should apply restrictions to cursor", async () => {
+    wasmTty.setInput("Yo");
+
+    wasmTty.setCursor(-1);
+    expect(wasmTty._cursor).toBe(0);
+
+    wasmTty.setCursor(100);
+    expect(wasmTty._cursor).toBe(2);
   });
 });
