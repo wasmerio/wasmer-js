8def6510d0a3a60c03036754c24a51dcad0fe85d
Syrus <me@syrusakbary.com>
2019-10-17 16:08:59 -0700

    Improved BigInt impl

--- packages/wasi/lib/polyfills/bigint.ts
+++ packages/wasi/lib/polyfills/bigint.ts
@@ -1,8 +1,8 @@
 // A very simple workaround for Big int. Works in conjunction with our custom
 // Dataview workaround at ./dataview.ts
 
-var commonjsGlobal = typeof globalThis !== "undefined" ? globalThis : {};
+const globalObj = typeof globalThis !== "undefined" ? globalThis : {};
 
 // @ts-ignore
-export const BigIntPolyfill: typeof BigInt = commonjsGlobal.BigInt || Number;
+export const BigIntPolyfill: typeof BigInt = globalObj.BigInt || Number;
 export type BigIntPolyfillType = bigint;
