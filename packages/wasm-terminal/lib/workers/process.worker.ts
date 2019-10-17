c7a53fde8246d064da562b9b36e1f206f0a52970
Syrus <me@syrusakbary.com>
2019-10-17 13:19:29 -0700

    Be explicit on the bindings for the worker

--- packages/wasm-terminal/lib/workers/process.worker.ts
+++ packages/wasm-terminal/lib/workers/process.worker.ts
@@ -3,5 +3,10 @@
 // Including comlink from source:
 // https://github.com/GoogleChromeLabs/comlink/issues/366
 import * as Comlink from "comlink";
+import WASI from "@wasmer/wasi";
+import BrowserWASIBindings from "@wasmer/wasi/bindings/browser";
+
 import Process from "../process/process";
+
+WASI.defaultBindings = BrowserWASIBindings;
 Comlink.expose(Process);
