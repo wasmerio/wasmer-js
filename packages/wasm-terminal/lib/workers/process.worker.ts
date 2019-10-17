ee6b33851cfecfbd22934b13e0444b722c0ab56e
Syrus <me@syrusakbary.com>
2019-10-17 13:51:55 -0700

    Fixed comlink

--- packages/wasm-terminal/lib/workers/process.worker.ts
+++ packages/wasm-terminal/lib/workers/process.worker.ts
@@ -1,7 +1,7 @@
 // Expoprt process as a worker
 // Including comlink from source:
 // https://github.com/GoogleChromeLabs/comlink/issues/366
-import * as Comlink from "comlink";
+import * as Comlink from "comlink/src/comlink";
 import Process from "../process/process";
 
 Comlink.expose(Process);
