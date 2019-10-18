8132aaa24f005b6adf3d0a194540cccac82aeb13
Syrus <me@syrusakbary.com>
2019-10-17 18:00:55 -0700

    Improved API a bit

--- examples/wasm-shell/components/wasm-terminal.tsx
+++ examples/wasm-shell/components/wasm-terminal.tsx
@@ -10,51 +10,13 @@ import WasmTerminal, {
 import WASI from "@wasmer/wasi";
 import BrowserWASIBindings from "@wasmer/wasi/bindings/browser";
 // @ts-ignore
-import { lowerI64Imports } from "@wasmer/wasm-transformer/unoptimized/wasm-transformer.esm.js";
-
-// Additional Command URLs
-// @ts-ignore
-// import stdinWasmUrl from "../../../crates/wasm_transformer/wasm_module_examples/stdin.wasm";
-// // @ts-ignore
-// import clockTimeGetUrl from "../../../crates/wasm_transformer/wasm_module_examples/clock_time_get.wasm";
-// // @ts-ignore
-// import pathOpenGetUrl from "../../../crates/wasm_transformer/wasm_module_examples/path_open.wasm";
-// // @ts-ignore
-// import twoImportsUrl from "../../../crates/wasm_transformer/wasm_module_examples/two-imports.wasm";
-// // @ts-ignore
-// import quickJsUrl from "../../../crates/wasm_transformer/wasm_module_examples/qjs.wasm";
-// // @ts-ignore
-// import dukTapeUrl from "../../../crates/wasm_transformer/wasm_module_examples/duk.wasm";
-// // @ts-ignore
-// import argtestUrl from "../../../crates/wasm_transformer/wasm_module_examples/argtest.wasm";
-// // @ts-ignore
-// import clangUrl from "../../../crates/wasm_transformer/wasm_module_examples/clang.wasm";
-// // @ts-ignore
-// import sqliteUrl from "../../../crates/wasm_transformer/wasm_module_examples/sqlite.wasm";
-// // @ts-ignore
-// import gettimeofdayUrl from "../../../crates/wasm_transformer/wasm_module_examples/gettimeofday/gettimeofday.wasm";
-// // @ts-ignore
-// import rsignUrl from "../../../crates/wasm_transformer/wasm_module_examples/rsign.wasm";
-// // @ts-ignore
-// import uutilsUrl from "../../../crates/wasm_transformer/wasm_module_examples/uutils.wasm";
+import { lowerI64Imports } from "@wasmer/wasm-transformer";
 
 import welcomeMessage from "./welcome-message";
 
 WASI.defaultBindings = BrowserWASIBindings;
 
 const commands = {
-  // a: stdinWasmUrl,
-  // c: clockTimeGetUrl,
-  // p: pathOpenGetUrl,
-  // g: gettimeofdayUrl,
-  // qjs: quickJsUrl,
-  // duk: dukTapeUrl,
-  // two: twoImportsUrl,
-  // arg: argtestUrl,
-  // clang: clangUrl,
-  // sqlite: sqliteUrl,
-  // rsign: rsignUrl,
-  // uutils: uutilsUrl,
   callback: (args: string[], stdin: string) => {
     return Promise.resolve(
       `Callback Command Working! Args: ${args}, stdin: ${stdin}`
