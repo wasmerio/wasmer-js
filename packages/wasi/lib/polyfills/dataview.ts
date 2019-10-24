8b221530c1b95f31e0ae4d1348b2bb2dcc393233
Syrus <me@syrusakbary.com>
2019-10-24 12:42:56 -0700

    Added getBigUint64 polyfill

--- packages/wasi/lib/polyfills/dataview.ts
+++ packages/wasi/lib/polyfills/dataview.ts
@@ -33,6 +33,30 @@ if (!exportedDataView.prototype.setBigUint64) {
     this.setUint32(byteOffset + (littleEndian ? 0 : 4), lowWord, littleEndian);
     this.setUint32(byteOffset + (littleEndian ? 4 : 0), highWord, littleEndian);
   };
+
+  exportedDataView.prototype.getBigUint64 = function(
+    byteOffset: number,
+    littleEndian: boolean | undefined
+  ): number {
+    let lowWord = this.getUint32(
+      byteOffset + (littleEndian ? 0 : 4),
+      littleEndian
+    );
+    let highWord = this.setUint32(
+      byteOffset + (littleEndian ? 4 : 0),
+      littleEndian
+    );
+
+    var lowWordAsBinaryStr = lowWord.toString(2);
+    var highWordAsBinaryStr = lowWord.toString(2);
+    // Convert the above binary str to 64 bit (actually 52 bit will work) by padding zeros in the left
+    var lowWordAsBinaryStrPadded = "";
+    for (var i = 0; i < 32 - lowWordAsBinaryStr.length; i++) {
+      lowWordAsBinaryStrPadded += "0";
+    }
+    lowWordAsBinaryStrPadded += lowWordAsBinaryStr;
+    return parseInt(highWordAsBinaryStr + lowWordAsBinaryStr);
+  };
 }
 
 export const DataViewPolyfill: any = exportedDataView;
