e4b456fb6e1e2fe4b94330b3e6f02ec665bbd029
Aaron Turner <aaron@aaronthedev.com>
2019-10-31 15:25:18 -0700

    What I have so far in helpoing syrus

--- e2e-tests/test/wasmfs.js
+++ e2e-tests/test/wasmfs.js
@@ -1,5 +1,8 @@
 const assert = require("assert");
 const fs = require("fs");
+const { TextDecoder } = require("util");
+const atob = require("atob");
+const btoa = require("btoa");
 const requireFromString = require("require-from-string");
 const puppeteer = require("puppeteer");
 
@@ -16,12 +19,35 @@ const testNodeBundle = async bundleString => {
 
   const BASE64_IMG =
     "iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mNk+M9QDwADhgGAWjR9awAAAABJRU5ErkJggg==";
-  const expectedBinary = new Uint8Array(new Buffer(BASE64_IMG, "base64"));
+  // const expectedBinary = new Uint8Array(new Buffer(BASE64_IMG, "base64"));
+  const expectedBinary = Uint8Array.from(atob(BASE64_IMG), c =>
+    c.charCodeAt(0)
+  );
+  console.log(BASE64_IMG.length, expectedBinary.length);
   wasmFs.fs.mkdirSync("/tmp/", { recursive: true });
-  wasmFs.volume.writeFileSync("/tmp/test.png", expectedBinary);
+  console.log("expected binary", expectedBinary);
+  console.log("Stringified expected binary", JSON.stringify(expectedBinary));
+  const expectedArray = Array.from(expectedBinary);
+  console.log("expected array", expectedArray);
+  wasmFs.volume.writeFileSync("/tmp/test.png", expectedArray);
 
-  console.log(wasmFs.toJSON()["/tmp/test.png"]);
-  if (wasmFs.toJSON()["/tmp/test.png"] !== BASE64_IMG) {
+  const savedString = wasmFs.toJSON()["/tmp/test.png"];
+  const savedArray = JSON.parse(`[${savedString}]`);
+  const savedBinary = Uint8Array.from(savedArray);
+
+  // Stuff breaks here
+  const decoder = new TextDecoder("utf8");
+  const decoded = decoder.decode(savedBinary);
+  const b64encoded = btoa(decoded);
+
+  console.log("savedString", savedString);
+  console.log("savedBinary", savedBinary);
+  console.log("decoded", decoded);
+  console.log("restored", b64encoded);
+  console.log("original", BASE64_IMG);
+
+  // Do the check
+  if (decoded !== BASE64_IMG) {
     throw new Error("Serialization doesn't work");
   }
 
--- packages/cli/README.md
+++ packages/cli/README.md
@@ -22,7 +22,7 @@ $ npm install -g @wasmer/cli
 $ wasmer-js COMMAND
 running command...
 $ wasmer-js (-v|--version|version)
-@wasmer/cli/0.4.1 darwin-x64 node-v12.6.0
+@wasmer/cli/0.4.1 darwin-x64 node-v12.7.0
 $ wasmer-js --help [COMMAND]
 USAGE
   $ wasmer-js COMMAND
