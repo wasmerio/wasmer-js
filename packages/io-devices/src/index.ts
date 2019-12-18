d57d3bbec374df6accb9c71f231844d77efc3686
Aaron Turner <aaron@aaronthedev.com>
2019-12-17 17:58:31 -0800

    Added a quick test for io-devices

--- packages/io-devices/src/index.ts
+++ packages/io-devices/src/index.ts
@@ -31,79 +31,64 @@ export default class IoDevicesDefault {
     this.bufferIndexDisplayCallback = () => {};
 
     // Open our directories and get their file descriptors
-    this.fdFrameBuffer = this.wasmFs.fs.openSync(FRAME_BUFFER, "w+");
-    this.fdWindowSize = this.wasmFs.fs.openSync(FRAME_BUFFER, "w+");
-    this.fdBufferIndexDisplay = this.wasmFs.fs.openSync(FRAME_BUFFER, "w+");
-    this.fdInput = this.wasmFs.fs.openSync(FRAME_BUFFER, "w+");
-
-    // TODO: Remove this test codeu
-    console.log(Object.keys(this.wasmFs.toJSON()));
-    console.log(this);
-    console.log(this.wasmFs.volume.fds);
-    console.log(Object.keys(this.wasmFs.volume.fds));
-    console.log(Object.keys(this.wasmFs.volume.fds[this.fdWindowSize]));
-    console.log(
-      "yooo",
-      this.wasmFs.volume.fds[this.fdWindowSize].node.read.toString()
+    this.fdFrameBuffer = this.wasmFs.volume.openSync(FRAME_BUFFER, "w+");
+    this.fdWindowSize = this.wasmFs.volume.openSync(WINDOW_SIZE, "w+");
+    this.fdBufferIndexDisplay = this.wasmFs.volume.openSync(
+      BUFFER_INDEX_DISPLAY,
+      "w+"
     );
+    this.fdInput = this.wasmFs.volume.openSync(INPUT, "w+");
 
     // Set up our read / write handlers
     const context = this;
     const originalInputRead = this.wasmFs.volume.fds[this.fdInput].node.read;
     // @ts-ignore
     this.wasmFs.volume.fds[this.fdInput].node.read = function() {
-      console.log("supppp");
       // @ts-ignore
-      const args = Array.prototype.slice(arguments);
-      originalInputRead.apply(
+      const args = Array.prototype.slice.call(arguments);
+      const response = originalInputRead.apply(
         context.wasmFs.volume.fds[context.fdInput].node,
         args as any
       );
       context._clearInput();
+      return response;
     };
-    // TODO: Functions are not being called? :(
-    // @ts-ignore
-    this.wasmFs.volume.fds[this.fdWindowSize].node.read = () =>
-      // @ts-ignore
-      console.log("yoooo");
     const originalWindowSizeWrite = this.wasmFs.volume.fds[this.fdWindowSize]
       .node.write;
     // @ts-ignore
     this.wasmFs.volume.fds[this.fdWindowSize].node.write = function() {
-      console.log("Yoooo");
       // @ts-ignore
-      const args = Array.prototype.slice(arguments);
-      originalWindowSizeWrite.apply(
+      const args = Array.prototype.slice.call(arguments);
+      const response = originalWindowSizeWrite.apply(
         context.wasmFs.volume.fds[context.fdInput].node,
         args as any
       );
       context.windowSizeCallback();
+      return response;
     };
     const originalBufferIndexDisplayWrite = this.wasmFs.volume.fds[
       this.fdBufferIndexDisplay
     ].node.write;
     // @ts-ignore
     this.wasmFs.volume.fds[this.fdBufferIndexDisplay].node.write = function() {
-      console.log("asjdhasdk");
       // @ts-ignore
-      const args = Array.prototype.slice(arguments);
-      originalBufferIndexDisplayWrite.apply(
+      const args = Array.prototype.slice.call(arguments);
+      const response = originalBufferIndexDisplayWrite.apply(
         context.wasmFs.volume.fds[context.fdInput].node,
         args as any
       );
       context.bufferIndexDisplayCallback();
+      return response;
     };
   }
 
   getFrameBuffer(): Uint8Array {
     const buffer = this.wasmFs.fs.readFileSync(FRAME_BUFFER);
-    console.log(buffer);
     return new Uint8Array();
   }
 
   getWindowSize(): Array<number> {
     const windowSizeBuffer = this.wasmFs.fs.readFileSync(WINDOW_SIZE);
-    console.log(windowSizeBuffer);
     const windowSize = new TextDecoder("utf-8").decode(windowSizeBuffer as any);
     const splitWindowSize = windowSize.split("x");
     return [parseInt(splitWindowSize[0], 10), parseInt(splitWindowSize[1], 10)];
@@ -119,9 +104,7 @@ export default class IoDevicesDefault {
 
   setInput(): void {}
 
-  _clearInput(): void {
-    console.log("Cleared input!");
-  }
+  _clearInput(): void {}
 }
 
 export const IoDevices = IoDevicesDefault;
