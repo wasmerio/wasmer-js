6185fb410262116808a18a93e76153775fe148f4
Aaron Turner <aaron@aaronthedev.com>
2020-01-06 16:16:54 -0800

    Mouse events should now be working

--- packages/wasm-terminal/src/io-device-window/io-device-window.ts
+++ packages/wasm-terminal/src/io-device-window/io-device-window.ts
@@ -11,12 +11,40 @@ export default class IoDeviceWindow {
   popupContainerElement: HTMLElement | undefined | null;
   popupImageData: any;
 
+  // Handle Key Press / Release
   oldPopupKeyCodes: Array<number> = [];
   popupKeyCodes: Array<number> = [];
 
+  // Handle Mouse Move
+  oldMouseMovePosition: { x: number; y: number } | undefined = { x: 0, y: 0 };
+  mouseMovePosition: { x: number; y: number } | undefined = { x: 0, y: 0 };
+
+  // Handle Mouse Clicks
+  oldMouseLeftClickPosition: { x: number; y: number } | undefined = {
+    x: 0,
+    y: 0
+  };
+  mouseLeftClickPosition: { x: number; y: number } | undefined = { x: 0, y: 0 };
+  oldMouseRightClickPosition: { x: number; y: number } | undefined = {
+    x: 0,
+    y: 0
+  };
+  mouseRightClickPosition: { x: number; y: number } | undefined = {
+    x: 0,
+    y: 0
+  };
+  oldMouseMiddleClickPosition: { x: number; y: number } | undefined = {
+    x: 0,
+    y: 0
+  };
+  mouseMiddleClickPosition: { x: number; y: number } | undefined = {
+    x: 0,
+    y: 0
+  };
+
   sharedIoDeviceInput: Int32Array | undefined;
 
-  constructor(sharedIoDeviceInputBuffer: SharedArrayBuffer | undefined) {
+  constructor(sharedIoDeviceInputBuffer?: SharedArrayBuffer) {
     if (sharedIoDeviceInputBuffer) {
       this.sharedIoDeviceInput = new Int32Array(sharedIoDeviceInputBuffer);
     }
@@ -29,7 +57,12 @@ export default class IoDeviceWindow {
       return;
     }
 
-    if (this.popupWindow && this.popupCanvas && this.popupCanvasContext) {
+    if (
+      this.popupWindow &&
+      this.popupContainerElement &&
+      this.popupCanvas &&
+      this.popupCanvasContext
+    ) {
       // Resize the open window
       this.popupWindow.resizeTo(width, height);
       this.popupContainerElement.style.width = `${width}px`;
@@ -47,8 +80,10 @@ export default class IoDeviceWindow {
   }
 
   close(): void {
-    this.popupWindow.close();
-    this.popupWindow = undefined;
+    if (this.popupWindow) {
+      this.popupWindow.close();
+      this.popupWindow = undefined;
+    }
   }
 
   drawRgbaArrayToFrameBuffer(rgbaArray: Uint8Array): void {
@@ -60,25 +95,86 @@ export default class IoDeviceWindow {
 
   getInputBuffer(): Uint8Array {
     // Handle keyCodes
-    const inputBuffer = [];
+    const inputBytes = new Uint8Array();
 
     // Key Presses
     this.popupKeyCodes.forEach(keyCode => {
       if (!this.oldPopupKeyCodes.includes(keyCode)) {
-        inputBuffer.push(1);
-        inputBuffer.push(keyCode);
+        inputBytes[inputBytes.length] = 1;
+        inputBytes[inputBytes.length] = keyCode;
       }
     });
+
+    // Mouse movement
+    if (
+      this.oldMouseMovePosition.x !== this.mouseMovePosition.x ||
+      this.oldMouseMovePosition.y !== this.mouseMovePosition.y
+    ) {
+      inputBytes[inputBytes.length] = 2;
+      this._append32BitIntToByteArray(this.mouseMovePosition.x, inputBytes);
+      this._append32BitIntToByteArray(this.mouseMovePosition.y, inputBytes);
+    }
+    this.oldMouseMovePosition = this.mouseMovePosition;
+
     // Key Releases
     this.oldPopupKeyCodes.forEach(keyCode => {
       if (!this.popupKeyCodes.includes(keyCode)) {
-        inputBuffer.push(3);
-        inputBuffer.push(keyCode);
+        inputBytes[inputBytes.length] = 3;
+        inputBytes[inputBytes.length] = keyCode;
       }
     });
     this.oldPopupKeyCodes = this.popupKeyCodes.slice(0);
 
-    const inputBytes = new Uint8Array(inputBuffer);
+    // Left Mouse Click
+    if (
+      this.oldMouseLeftClickPosition.x !== this.mouseLeftClickPosition.x ||
+      this.oldMouseLeftClickPosition.y !== this.mouseLeftClickPosition.y
+    ) {
+      inputBytes[inputBytes.length] = 4;
+      this._append32BitIntToByteArray(
+        this.mouseLeftClickPosition.x,
+        inputBytes
+      );
+      this._append32BitIntToByteArray(
+        this.mouseLeftClickPosition.y,
+        inputBytes
+      );
+    }
+    this.oldMouseLeftClickPosition = this.mouseLeftClickPosition;
+
+    // Right Mouse Click
+    if (
+      this.oldMouseRightClickPosition.x !== this.mouseRightClickPosition.x ||
+      this.oldMouseRightClickPosition.y !== this.mouseRightClickPosition.y
+    ) {
+      inputBytes[inputBytes.length] = 5;
+      this._append32BitIntToByteArray(
+        this.mouseRightClickPosition.x,
+        inputBytes
+      );
+      this._append32BitIntToByteArray(
+        this.mouseRightClickPosition.y,
+        inputBytes
+      );
+    }
+    this.oldMouseRightClickPosition = this.mouseRightClickPosition;
+
+    // Middle Mouse Click
+    if (
+      this.oldMouseMiddleClickPosition.x !== this.mouseMiddleClickPosition.x ||
+      this.oldMouseMiddleClickPosition.y !== this.mouseMiddleClickPosition.y
+    ) {
+      inputBytes[inputBytes.length] = 7;
+      this._append32BitIntToByteArray(
+        this.mouseMiddleClickPosition.x,
+        inputBytes
+      );
+      this._append32BitIntToByteArray(
+        this.mouseMiddleClickPosition.y,
+        inputBytes
+      );
+    }
+    this.oldMouseMiddleClickPosition = this.mouseMiddleClickPosition;
 
     if (this.sharedIoDeviceInput) {
       // Write the buffer to the memory
@@ -113,6 +209,8 @@ export default class IoDeviceWindow {
           align-items: center;
 
           margin: 0px;
+          margin-left: auto;
+          margin-right: auto;
         }
 
         .container {
@@ -188,6 +286,16 @@ export default class IoDeviceWindow {
     );
   }
 
+  _append32BitIntToByteArray(value: number, byteArray: Uint8Array) {
+    for (let i = 0; i < 4; i++) {
+      // Goes smallest to largest (little endian)
+      let currentByte = value;
+      currentByte = currentByte & (0x0f << (i * 8));
+      currentByte = currentByte >> (i * 8);
+      byteArray[byteArray.length] = currentByte;
+    }
+  }
+
   _eventListenerKeydown(event: KeyboardEvent): void {
     const keyCode = event.keyCode;
     this.popupKeyCodes.push(event.keyCode);
@@ -202,10 +310,69 @@ export default class IoDeviceWindow {
   }
 
   _eventListenerMousemove(event: MouseEvent): void {
-    console.log(event);
+    const position = this._getPositionFromMouseEvent(event);
+
+    if (position === undefined) {
+      return;
+    }
+
+    this.mouseMovePosition = position;
   }
 
   _eventListenerClick(event: MouseEvent): void {
-    console.log(event);
+    const position = this._getPositionFromMouseEvent(event);
+
+    if (position === undefined) {
+      return;
+    }
+
+    if (event.button === 0) {
+      // Left click
+      this.mouseLeftClickPosition = position;
+    } else if (event.button === 1) {
+      // Middle click
+      this.mouseMiddleClickPosition = position;
+    } else if (event.button === 2) {
+      // Right click
+      this.mouseRightClickPosition = position;
+    }
+  }
+
+  _getPositionFromMouseEvent(
+    event: MouseEvent
+  ): { x: number; y: number } | undefined {
+    if (!this.popupContainerElement) {
+      return undefined;
+    }
+
+    const popupContainerBoundingClientRect = this.popupContainerElement.getBoundingClientRect();
+    const minX = popupContainerBoundingClientRect.x;
+    const maxX =
+      popupContainerBoundingClientRect.x +
+      popupContainerBoundingClientRect.width;
+    const minY = popupContainerBoundingClientRect.y;
+    const maxY =
+      popupContainerBoundingClientRect.y +
+      popupContainerBoundingClientRect.height;
+
+    let x = undefined;
+    let y = undefined;
+
+    if (event.x >= minX && event.x <= maxX) {
+      x = event.x;
+    }
+
+    if (event.y >= minY && event.y <= maxY) {
+      y = event.y;
+    }
+
+    if (x === undefined || y === undefined) {
+      return undefined;
+    }
+
+    return {
+      x,
+      y
+    };
   }
 }
