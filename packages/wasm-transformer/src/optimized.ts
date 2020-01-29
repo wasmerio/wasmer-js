45fec93943dcb7d73d6f71790ab3b9bc134b8968
Aaron Turner <aaron@aaronthedev.com>
2020-01-29 13:12:53 -0800

    Added support for i64 feature detection

--- packages/wasm-transformer/src/optimized.ts
+++ packages/wasm-transformer/src/optimized.ts
@@ -1,10 +1,25 @@
-export * from "../wasm-pack/web/wasm_transformer";
-
 // @ts-ignore
-import init from "../wasm-pack/web/wasm_transformer";
+import * as wasmTransformerWasmPack from "../wasm-pack/web/wasm_transformer";
+
+import { bigInt } from "wasm-feature-detect";
+
+let isBigIntSupported = false;
 
 export const wasmTransformerInit = async (passedWasmTransformerUrl: string) => {
-  await init(passedWasmTransformerUrl);
+  await wasmTransformerWasmPack.default(passedWasmTransformerUrl);
+  isBigIntSupported = await bigInt();
+};
+
+export const lowerI64Imports = (wasmBinary: Uint8Array) => {
+  if (isBigIntSupported) {
+    return wasmBinary;
+  }
+
+  return wasmTransformerWasmPack.lowerI64Imports(wasmBinary);
+};
+
+export const version = () => {
+  return wasmTransformerWasmPack.version();
 };
 
 export default wasmTransformerInit;
