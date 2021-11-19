8806d1456f4a6c95d827cf08176656dd665c1f90
Syrus Akbary <me@syrusakbary.com>
2021-11-19 11:30:20 +0100

    improved a bit the bundler

--- lib.js
+++ lib.js
@@ -1,1 +1,4 @@
-export * from "./pkg/index";
+export { WASI } from "./pkg/index";
+
+// pkg.init()
+// let wasi = new WASI({})
--- webpack.config.js
+++ webpack.config.js
@@ -2,14 +2,22 @@ const path = require('path')
 const WasmPackPlugin = require('@wasm-tool/wasm-pack-plugin')
 
 module.exports = {
-    mode: 'development',
+    // mode: 'development',
+    mode: 'production',
     entry: './lib.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js',
     },
+    module: {
+        // rules: [{
+        //     test: /\.wasm/,
+        //     type: 'asset/inline'
+        // }]
+    },
     plugins: [
         new WasmPackPlugin({
+            extraArgs: '--target web',
             crateDirectory: path.resolve(__dirname, '.'),
         }),
     ],
