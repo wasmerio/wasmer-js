aaa42162feee85f62b3796945100a01a3ebfab1c
Syrus Akbary <me@syrusakbary.com>
2021-12-07 14:23:51 +0100

    Fixed deno imports

--- wasi.ts
+++ wasi.ts
@@ -1,7 +1,7 @@
 // @deno-types="./pkg/wasmer_wasi_js.d.ts"
 import baseInit, { WASI, InitInput } from "./pkg/wasmer_wasi_js.js";
 // @deno-types="./pkg/wasmer_wasi_js.d.ts"
-export { WASI, MemFS, JSVirtualFile, WasmerRuntimeError, InitInput } from "./pkg/wasmer_wasi_js.js";
+export { WASI, MemFS, JSVirtualFile, WasmerRuntimeError } from "./pkg/wasmer_wasi_js.js";
 
 let inited: Promise<any> | null = null;
 export const init = async (input?: InitInput | Promise<InitInput>, force?: boolean) => {
