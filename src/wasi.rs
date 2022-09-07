80dc150b99e4119418096206fb4fd2cbc99d3575
Shachar Itzhaky <corwin.amber@gmail.com>
2022-09-07 14:12:57 +0300

    [feature] Allow setting fs in WASI config.
    Using some boilerplate to get the instance from a JS object.

--- src/wasi.rs
+++ src/wasi.rs
@@ -6,6 +6,8 @@ use wasmer::{Imports, Instance, Module, Store};
 use wasmer_wasi::Pipe;
 use wasmer_wasi::{Stderr, Stdin, Stdout, WasiError, WasiFunctionEnv, WasiState};
 
+use wasm_bindgen::convert::FromWasmAbi;
+
 #[wasm_bindgen]
 pub struct WASI {
     store: Store,
@@ -86,9 +88,7 @@ impl WASI {
             if fs.is_undefined() {
                 MemFS::new()?
             } else {
-                MemFS::new()?
-                // let mem_fs: MemFS = fs.dyn_into()?;
-                // mem_fs
+                MemFS::from_js(fs)?
             }
         };
         let mut store = Store::default();
@@ -324,5 +324,20 @@ impl WASI {
     #[wasm_bindgen(js_name = setStdinString)]
     pub fn set_stdin_string(&mut self, input: String) -> Result<(), JsValue> {
         self.set_stdin_buffer(input.as_bytes())
+    }
+}
+
+// helper function for passing Rust objects through JS
+// https://github.com/rustwasm/wasm-bindgen/issues/2231#issuecomment-656293288
+pub fn generic_of_jsval<T: FromWasmAbi<Abi=u32>>(js: JsValue, classname: &str) -> Result<T, JsValue> {
+    use js_sys::{Object, Reflect};
+    let ctor_name = Object::get_prototype_of(&js).constructor().name();
+    if ctor_name == classname {
+        let ptr = Reflect::get(&js, &JsValue::from_str("ptr"))?;
+        let ptr_u32: u32 = ptr.as_f64().ok_or(JsValue::NULL)? as u32;
+        let foo = unsafe { T::from_abi(ptr_u32) };
+        Ok(foo)
+    } else {
+        Err(JsValue::NULL)
     }
 }
