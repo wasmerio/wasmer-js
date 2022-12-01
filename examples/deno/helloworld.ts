087e615fc3da7de8e58af5d4044d3e10ed4e7569
Nbiba Bedis <bedisnbiba@gmail.com>
2022-12-01 15:25:21 +0100

    update deno examples

--- examples/deno/helloworld.ts
+++ examples/deno/helloworld.ts
@@ -1,18 +1,20 @@
-import { init, WASI } from 'https://deno.land/x/wasm/wasi.ts';
+import { init, WASI } from "https://deno.land/x/wasm/wasi.ts";
 
 // This is needed to load the WASI library first
 await init();
 
-let wasi = new WASI({
+const wasi = new WASI({
   env: {},
   args: [],
 });
 
-const moduleBytes = fetch("https://cdn.deno.land/wasm/versions/v1.0.1/raw/tests/demo.wasm");
+const moduleBytes = fetch(
+  "https://cdn.deno.land/wasm/versions/v1.0.2/raw/tests/demo.wasm",
+);
 const module = await WebAssembly.compileStreaming(moduleBytes);
 await wasi.instantiate(module, {});
 
-let exitCode = wasi.start();
-let stdout = wasi.getStdoutString();
- // This should print "hello world (exit code: 0)"
+const exitCode = wasi.start();
+const stdout = wasi.getStdoutString();
+// This should print "hello world (exit code: 0)"
 console.log(`${stdout}(exit code: ${exitCode})`);
