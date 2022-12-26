1eab9d7d5d62a7cdc5003f758b097c223287fd43
Daniel Nalborczyk <dnalborczyk@gmail.com>
2022-12-25 22:32:40 -0500

    use consistent const

--- examples/node/fs.mjs
+++ examples/node/fs.mjs
@@ -4,12 +4,12 @@ import { init, WASI } from "@wasmer/wasi";
 // This is needed to load the WASI library first
 await init();
 
-let wasi = new WASI({
+const wasi = new WASI({
   env: {},
   args: [],
 });
 
-const buf = await readFile('../../tests/mapdir.wasm');
+const buf = await readFile("../../tests/mapdir.wasm");
 
 const module = await WebAssembly.compile(buf);
 wasi.instantiate(module, {});
@@ -17,12 +17,12 @@ wasi.instantiate(module, {});
 wasi.fs.createDir("/a");
 wasi.fs.createDir("/b");
 
-let file = wasi.fs.open("/file", {read: true, write: true, create: true});
+const file = wasi.fs.open("/file", { read: true, write: true, create: true });
 file.writeString("fileContents");
 file.seek(0);
 
-let exitCode = wasi.start();
-let stdout = wasi.getStdoutString();
+const exitCode = wasi.start();
+const stdout = wasi.getStdoutString();
 
 // This should print "hello world (exit code: 0)"
 console.log(`${stdout}(exit code: ${exitCode})`);
--- examples/node/helloworld.mjs
+++ examples/node/helloworld.mjs
@@ -4,17 +4,17 @@ import { init, WASI } from "@wasmer/wasi";
 // This is needed to load the WASI library first
 await init();
 
-let wasi = new WASI({
+const wasi = new WASI({
   env: {},
   args: [],
 });
 
-const buf = await readFile('../../tests/demo.wasm');
+const buf = await readFile("../../tests/demo.wasm");
 
 const module = await WebAssembly.compile(buf);
 wasi.instantiate(module, {});
 
-let exitCode = wasi.start();
-let stdout = wasi.getStdoutString();
+const exitCode = wasi.start();
+const stdout = wasi.getStdoutString();
 // This should print "hello world (exit code: 0)"
 console.log(`${stdout}(exit code: ${exitCode})`);
