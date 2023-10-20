e6e5cf3a9dfcaa51845fe56fabddab8158fb9f9d
Michael-F-Bryan <michael@wasmer.io>
2023-10-20 18:11:52 +0800

    Cleaned up the example code

--- examples/wasmer.sh/index.ts
+++ examples/wasmer.sh/index.ts
@@ -1,30 +1,24 @@
 import "xterm/css/xterm.css";
 
-import { SpawnConfig, Tty, Wasmer, init } from "@wasmer/wasix";
+import { Instance, SpawnConfig, Tty, Wasmer, init } from "@wasmer/wasix";
 import { Terminal } from "xterm";
 import { FitAddon } from "xterm-addon-fit";
 
 const encoder = new TextEncoder();
 
-const packageName = "sharrattj/bash";
-const args: string[] = [];
-const uses: string[] = ["sharrattj/coreutils"];
-
 async function main() {
     await init();
 
+    // Create a terminal
     const term = new Terminal({ cursorBlink: true, convertEol: true });
     const fit = new FitAddon();
     term.loadAddon(fit);
-
-    const element = document.getElementById("app")!;
-    term.open(element);
+    term.open(document.getElementById("terminal")!);
     fit.fit();
 
-    term.writeln("Starting...");
     const wasmer = new Wasmer();
 
-    // Attach the TTY
+    // Create a TTY and attach it to the terminal
     const tty = new Tty();
     tty.state = {...tty.state, cols: term.cols, rows: term.rows};
     term.onResize(({cols, rows}) => {
@@ -32,58 +26,34 @@ async function main() {
     });
     wasmer.runtime().set_tty(tty);
 
+    term.writeln("Starting...");
+
     while (true) {
-        await runInstance(term, wasmer, packageName, { args, uses });
-        term.writeln("Rebooting...");
-    }
-}
+        const instance = await wasmer.spawn("sharrattj/bash", {
+            args: [],
+            uses: ["python/python@0.1.0"],
+        });
 
-async function runInstance(term: Terminal, wasmer: Wasmer, packageName: string, config: SpawnConfig) {
-    const instance = await wasmer.spawn(packageName, config);
-    term.clear();
+        // Connect stdin/stdout/stderr to the terminal
+        const stdin: WritableStreamDefaultWriter<Uint8Array> = instance.stdin!.getWriter();
+        term.onData(line => { stdin.write(encoder.encode(line)); });
+        copyStream(instance.stdout, term);
+        copyStream(instance.stderr, term);
 
-    const stdin: WritableStreamDefaultWriter<Uint8Array> = instance.stdin!.getWriter();
-    term.onData(line => { stdin.write(encoder.encode(line)); });
+        // Now, wait until bash exits
+        const { code } = await instance.wait();
 
-    const stdout: ReadableStreamDefaultReader<Uint8Array> = instance.stdout.getReader();
-    copyStream(stdout, line => writeMultiline(term, line));
-
-    const stderr: ReadableStreamDefaultReader<Uint8Array> = instance.stderr.getReader();
-    copyStream(stderr, line => writeMultiline(term, line));
-
-    const { code } = await instance.wait();
-
-    if (code != 0) {
-        term.writeln(`\nExit code: ${code}`);
-    }
-}
-
-async function copyStream(reader: ReadableStreamDefaultReader<Uint8Array>, cb: (line: string) => void) {
-    const decoder = new TextDecoder("utf-8");
-
-    while(true) {
-        const {done, value} = await reader.read();
-
-        if (done || !value) {
-            break;
-        }
-        const chunk = decoder.decode(value);
-        cb(chunk);
-    }
-}
-
-function writeMultiline(term: Terminal, text: string) {
-    term.write(text);
-    return;
-    const lines = text.split("\n").map(l => l.trimEnd());
-
-    if (lines.length == 1) {
-        term.write(text);
-    } else {
-        for (const line of lines) {
-            term.writeln(line);
+        if (code != 0) {
+            term.writeln(`\nExit code: ${code}`);
+            term.writeln("Rebooting...");
         }
     }
+}
+
+async function copyStream(reader: ReadableStream, term: Terminal) {
+    const writer = new WritableStream({ write: chunk => term.write(chunk) });
+    reader.pipeTo(writer);
+
 }
 
 addEventListener("DOMContentLoaded", () => main());
