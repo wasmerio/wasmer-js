07e796279f4f911996eeaf4a0527b595dd4bda67
Michael-F-Bryan <michael@wasmer.io>
2023-11-02 16:15:03 +0800

    Make logger initialization a separate operation

--- src/lib.rs
+++ src/lib.rs
@@ -28,7 +28,7 @@ use tracing_subscriber::{layer::SubscriberExt, EnvFilter};
 use wasm_bindgen::prelude::wasm_bindgen;
 
 pub(crate) const USER_AGENT: &str = concat!(env!("CARGO_PKG_NAME"), "/", env!("CARGO_PKG_VERSION"));
-const RUST_LOG: &[&str] = &["warn", "wasmer_wasix=info", "wasmer_wasix_js=info"];
+const DEFAULT_RUST_LOG: &[&str] = &["warn"];
 
 #[wasm_bindgen]
 pub fn wat2wasm(wat: JsString) -> Result<Uint8Array, utils::Error> {
@@ -40,13 +40,47 @@ pub fn wat2wasm(wat: JsString) -> Result<Uint8Array, utils::Error> {
 #[wasm_bindgen(start, skip_typescript)]
 fn on_start() {
     console_error_panic_hook::set_once();
+}
 
+/// Initialize the logger used by `@wasmer/wasix`.
+///
+/// This function can only be called once. Subsequent calls will raise an
+/// exception.
+///
+/// ## Filtering Logs
+///
+/// The `filter` string can be used to tweak logging verbosity, both globally
+/// or on a per-module basis, and follows [the `$RUST_LOG` format][format].
+///
+/// Some examples:
+/// - `off` - turn off all logs
+/// - `error`, `warn`, `info`, `debug`, `trace` - set the global log level
+/// - `wasmer_wasix` - enable logs for `wasmer_wasix`
+/// - `info,wasmer_wasix_js::package_loader=trace` - set the global log level to
+///   `info` and set `wasmer_wasix_js::package_loader` to `trace`
+/// - `wasmer_wasix_js=debug/flush` -  turns on debug logging for
+///   `wasmer_wasix_js` where the log message includes `flush`
+/// - `warn,wasmer=info,wasmer_wasix::syscalls::wasi=trace` - directives can be
+///   mixed arbitrarily
+///
+/// When no `filter` string is provided, a useful default will be used.
+///
+/// [format]: https://docs.rs/tracing-subscriber/latest/tracing_subscriber/filter/struct.EnvFilter.html#directives
+#[wasm_bindgen(js_name = "initializeLogger")]
+pub fn initialize_logger(filter: Option<String>) -> Result<(), utils::Error> {
     let max_level = tracing::level_filters::STATIC_MAX_LEVEL
         .into_level()
         .unwrap_or(tracing::Level::ERROR);
 
+    let filter = EnvFilter::builder()
+        .with_regex(false)
+        .with_default_directive(max_level.into())
+        .parse_lossy(filter.unwrap_or_else(|| DEFAULT_RUST_LOG.join(",")));
+
     let registry = tracing_subscriber::Registry::default()
-        .with(EnvFilter::new(RUST_LOG.join(",")))
+        .with(filter)
         .with(tracing_browser_subscriber::BrowserLayer::new().with_max_level(max_level));
-    tracing::subscriber::set_global_default(registry).unwrap();
+    tracing::subscriber::set_global_default(registry)?;
+
+    Ok(())
 }
