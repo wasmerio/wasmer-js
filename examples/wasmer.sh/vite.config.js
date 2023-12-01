50fc71f91a6606c5db24756374b1c70d757d6dd5
Michael-F-Bryan <michael@wasmer.io>
2023-12-01 14:02:19 +0800

    Rewrote the wasmer.sh example to be simpler and use Vite

---
+++ examples/wasmer.sh/vite.config.js
@@ -0,0 +1,10 @@
+import { defineConfig } from "vite";
+
+export default defineConfig({
+    server: {
+        headers: {
+            "Cross-Origin-Opener-Policy": "same-origin",
+            "Cross-Origin-Embedder-Policy": "require-corp",
+        },
+    },
+});
