9b3766b8736e13c792d70a4d1cac7a14211b68e5
Syrus Akbary <me@syrusakbary.com>
2023-12-12 18:09:51 -0800

    Multiple fixes

--- lib_bundled.ts
+++ lib_bundled.ts
@@ -1,6 +1,7 @@
 export * from "./lib";
 // @ts-ignore
 import { init as load, InitInput, InitOutput, ThreadPoolWorker } from "./lib";
+// @ts-ignore
 import wasm_bytes from "./pkg/wasmer_js_bg.wasm";
 
 
@@ -8,9 +9,10 @@ import wasm_bytes from "./pkg/wasmer_js_bg.wasm";
  * Initialize the underlying WebAssembly module.
  */
 
-export const init = (module_or_path?: InitInput | Promise<InitInput>, maybe_memory?: WebAssembly.Memory): Promise<InitOutput> => {
+export const init = async (module_or_path?: InitInput | Promise<InitInput>, maybe_memory?: WebAssembly.Memory): Promise<InitOutput> => {
     if (!module_or_path) {
-        module_or_path = wasm_bytes;
+        // @ts-ignore
+        module_or_path = await wasm_bytes();
     }
     return load(module_or_path, maybe_memory);
 }
