aff00698bbd98960cce131289cbc01f4febe7ce0
Michael-F-Bryan <michael@wasmer.io>
2023-12-21 01:17:29 +0800

    Added a download button so people can download the converted file

--- examples/ffmpeg-react/src/App.tsx
+++ examples/ffmpeg-react/src/App.tsx
@@ -226,7 +226,17 @@ export default function App() {
                 className="rounded-md bg-indigo-50 px-3.5 py-2.5 text-sm font-semibold shadow-sm hover:bg-indigo-100"
             >
                 <ProcessingStatus status={processingStatus} />
+
             </button>
+
+            {
+                outputVideo.fileSrc &&
+                <a
+                    download={outputVideo.file}
+                    href={outputVideo.fileSrc}
+                    className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+                >Download</a>
+            }
         </main>
     );
 }
