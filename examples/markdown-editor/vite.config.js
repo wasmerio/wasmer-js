a7c3ba015b046a3c8951371a4471ea2644ef60d4
Michael-F-Bryan <michael@wasmer.io>
2023-12-20 16:24:12 +0800

    Wire up vite to automatically compile the markdown-renderer crate

--- examples/markdown-editor/vite.config.js
+++ examples/markdown-editor/vite.config.js
@@ -1,4 +1,5 @@
 import { defineConfig } from "vite";
+import { exec } from "node:child_process";
 
 export default defineConfig({
     server: {
@@ -7,5 +8,26 @@ export default defineConfig({
             "Cross-Origin-Embedder-Policy": "require-corp",
         },
     },
+    plugins: [
+        {
+            name: "cargo-build",
+            buildStart: () => {
+                return new Promise((resolve, reject) => {
+                    exec(
+                        "cargo build --target=wasm32-wasi --manifest-path=markdown-renderer/Cargo.toml --release --quiet",
+                        (err, stdout, stderr) => {
+                            if (err) {
+                                console.log("Stdout:", stdout);
+                                console.log("Stderr:", stderr);
+                                reject(err);
+                            } else {
+                                resolve();
+                            }
+                        }
+                    );
+                });
+            }
+        },
+    ]
 });
 
