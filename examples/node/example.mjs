7334f3c6d6b4cce55ce88d56f1cfc645626e84c9
Syrus Akbary <me@syrusakbary.com>
2024-10-31 17:17:06 +0100

    Improved Node example

--- examples/node/example.mjs
+++ examples/node/example.mjs
@@ -1,45 +1,37 @@
-// import fs from "node:fs";
-import process from "node:process";
-import { init, Wasmer } from "../../dist/node.mjs"
+import { init, Wasmer } from "@wasmer/sdk";
+// import process from "node:process";
 
-const encoder = new TextEncoder();
-const decoder = new TextDecoder();
+// const encoder = new TextEncoder();
+// const decoder = new TextDecoder();
 
-async  function connectStreams(instance) {
-// process.stdin.
-const stdin = instance.stdin?.getWriter();
-process.stdin.resume();
+// async function connectStreams(instance) {
+//   const stdin = instance.stdin?.getWriter();
+//   process.stdin.resume();
 
-process.stdin.on("data", data => stdin?.write(encoder.encode(data)));
+//   process.stdin.on("data", data => stdin?.write(encoder.encode(data)));
 
-  instance.stdout.pipeTo(
-    new WritableStream({ write: chunk => process.stdout.write(decoder.decode(chunk)) }),
-  );
-  instance.stderr.pipeTo(
-    new WritableStream({ write: chunk => process.stderr.write(decoder.decode(chunk)) }),
-  );
-}
-
-// let path = new URL("../../dist/node.,js", import.meta.url);
-// let wasmPath = new URL("../../dist/wasmer_js_bg.wasm", import.meta.url);
-// let wasmFile = fs.readFileSync(wasmPath);
-
-
-// Replace __filename with "/Users/syrusakbary/Development/wasmer-js/node_modules/.deno/web-worker@1.3.0/node_modules/web-worker/node.js"
+//   instance.stdout.pipeTo(
+//     new WritableStream({
+//       write: chunk => process.stdout.write(decoder.decode(chunk)),
+//     }),
+//   );
+//   instance.stderr.pipeTo(
+//     new WritableStream({
+//       write: chunk => process.stderr.write(decoder.decode(chunk)),
+//     }),
+//   );
+// }
 
 async function run() {
-    await init({log: "trace"});
-    // await init();
+  await init();
 
-let cowsay = await Wasmer.fromRegistry("cowsay")
+  let cowsay = await Wasmer.fromRegistry("cowsay");
 
-let instance = await cowsay.entrypoint.run({args: ["hello world"]});
-// let instance = await cowsay.entrypoint.run({args: []});
+  let instance = await cowsay.entrypoint.run({ args: ["hello world"] });
 
-await connectStreams(instance);
-// const output = await instance.wait();
-// console.log(output.stdout);
-
+  // await connectStreams(instance);
+  const output = await instance.wait();
+  console.log(output.stdout);
 }
 
 run();
