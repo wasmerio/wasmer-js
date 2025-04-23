92100ab2d30c0b7473ba0292687fa25f8996c15f
Syrus Akbary <me@syrusakbary.com>
2025-04-23 15:22:52 +0200

    Iterated further

--- sdk-lite/examples/wordpress/index.js
+++ sdk-lite/examples/wordpress/index.js
@@ -18,12 +18,22 @@ const autobuildApp = await Wasmer.autobuildApp({
     // domains: ["testoftheapp.wasmer.dev"],
     jobs: [
         // {command: "bash", cliArgs: ["-c", `echo "Hello for demo"`]},
-        {command: "bash", cliArgs: ["-c", `
+        {
+          command: "bash", cliArgs: ["-c", `
           echo "Installing plugins... $STATIC_STUDIO_API_KEY";
-          php /app/wp-cli.phar --allow-root --path=/app plugin install https://api.static.studio/storage/v1/object/public/plugins/simply-static-pro.zip;
-          php /app/wp-cli.phar --allow-root --path=/app plugin install https://api.static.studio/storage/v1/object/public/plugins/simply-static-studio-helper.zip;
+          php /app/wp-cli.phar --allow-root --path=/app plugin install https://api.static.studio/storage/v1/object/public/plugins/simply-static-pro.zip || true;
+          php /app/wp-cli.phar --allow-root --path=/app plugin install https://api.static.studio/storage/v1/object/public/plugins/simply-static-studio-helper.zip || true;
           echo "Plugins installed";
-          `]},
+          `]
+          // command: "bash", cliArgs: ["-c", `
+          //   echo "Setting up MU plugin";
+          //   mkdir -p /app/wp-content/mu-plugins;
+          //   curl -L https://api.static.studio/storage/v1/object/public/plugins/load.php -o /app/wp-content/mu-plugins/load.php;
+          //   curl -L https://api.static.studio/storage/v1/object/public/plugins/simply-static-studio-helper.zip -o /app/wp-content/mu-plugins/simply-static-studio-helper.zip;
+          //   unzip /app/wp-content/mu-plugins/simply-static-studio-helper.zip;
+          //   echo "MU plugin installed.";
+          //   `]
+        },
     ],
     secrets: [
         {name: "STATIC_STUDIO_API_KEY", value: "static-studio-api-key"},
